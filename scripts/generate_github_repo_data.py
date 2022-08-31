1dd3f0d47680e0f4fc08da643d3fc27ebf4ccb70
ucancallmealicia <alicia.detelich@yale.edu>
2022-08-31 11:03:40 -0400

    docs

--- scripts/generate_github_repo_data.py
+++ scripts/generate_github_repo_data.py
@@ -9,19 +9,24 @@ Same with data auditing and validation stuff
 '''
 
 import csv
-import pprint
+import json
+from operator import itemgetter
 import requests
 
 
-def convert_to_csv(repo_data):
+def convert_to_csv(repo_data, inclusions, exclusions):
     return [[repo.get('owner').get('login'),
              repo.get('name'),
              repo.get('html_url'),
              repo.get('description'),
              repo.get('created_at'),
-             repo.get('updated_at')
+             repo.get('updated_at'),
+             #repo.get('fork')
                 ] for repo in repo_data 
-                  if repo.get('fork') == False
+                  if (repo.get('fork') == False or 
+                      repo.get('name') in inclusions
+                     )
+                  if repo.get('name') not in exclusions
                   and int(repo.get('updated_at').split('-')[0]) > 2016
             ]
 
@@ -31,32 +36,26 @@ def get_github_repo_data(username):
     github_data = requests.get(github_uri).json()
     return github_data
 
-def get_github_report(usernames):
+def get_github_report(usernames, inclusions, exclusions):
     main_csv = []
     for username in usernames:
         github_data = get_github_repo_data(username)
-        github_csv =  convert_to_csv(github_data)
+        github_csv =  convert_to_csv(github_data, inclusions, exclusions)
         main_csv.extend(github_csv)
-    return main_csv
+    sorted_main_csv = sorted(main_csv, key=itemgetter(5), reverse=True)
+    return sorted_main_csv
 
 
 def main():
-    # set these in a config
-    usernames = ['ucancallmealicia', 'yalemssa', 'YaleArchivesSpace']
-    github_repo_csv = get_github_report(usernames)
-    output_fp = input('Please enter path to output CSV: ')
-    fields = ['owner', 'repository', 'url', 'description', 'created', 'updated']
-    with open(output_fp, 'a', encoding='utf8') as outfile:
-        writer = csv.writer(outfile)
-        writer.writerow(fields)
-        writer.writerows(github_repo_csv)
+    with open('config.json', encoding='utf8') as json_file:
+        config = json.load(json_file)
+        github_repo_csv = get_github_report(config.get('usernames'), config.get('inclusions'), config.get('exclusions'))
+        fields = ['owner', 'repository', 'url', 'description', 'created', 'updated']
+        with open(config.get('output'), 'w', encoding='utf8') as outfile:
+            writer = csv.writer(outfile)
+            writer.writerow(fields)
+            writer.writerows(github_repo_csv)
 
 
 if __name__ == '__main__':
     main()
-
-# TODO
-# need a way to dedupe forked repos, etc.
-
-
-
