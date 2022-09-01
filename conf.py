f39aee6637726e7865fa835afe036a0e8b7a33dc
ucancallmealicia <alicia.detelich@yale.edu>
2022-09-01 11:42:58 -0400

    fix docs bug

--- conf.py
+++ conf.py
@@ -13,7 +13,6 @@
 
 import sys, os
 import sphinx_rtd_theme
-import sphinx_mdinclude
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
